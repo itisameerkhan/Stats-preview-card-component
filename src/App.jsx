bc877465204d609e92ddb2b8b51de286a5ad0cf0
itisameerkhan <itisameerkhan@gmail.com>
2023-12-15 11:50:46 +0530

    15/12/2023 11:50

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import '../images/image-header-desktop.jpg'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app-container">
+       <div className="app-left">
+          <h1>Get <span>insights</span> that help your business grow.</h1>
+          <p>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
+          <div className="app-left-inner">
+            <div className="div-element">
+              <h1>10k+</h1>
+              <p>COMPANIES</p>
+            </div>
+            <div className="div-element">
+              <h1>314</h1>
+              <p>TEMPLATES</p>
+            </div>
+            <div className="div-element">
+              <h1>12M+</h1>
+              <p>QUERIES</p>
+            </div>
+          </div>
+       </div>
+       <div className="app-right">
+        <div className="overlay"></div>
+       </div>
+    </div>
   )
 }
 
-export default App
+export default App;
